30716f8106d2d16c5a3f7b812841ec11bef791f5
Elandruf <andreseduardo2710@gmail.com>
2024-11-26 14:49:30 +0000

    rf

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,7 +8,7 @@ function Home() {
 	const [userData, setUserData] = useState();
 	const url = 'https://playground.4geeks.com/todo';
 
-
+	//GET//
 
 	const getDataAsync = async () => {
 		try {
@@ -23,92 +23,77 @@ function Home() {
 		}
 
 	}
-	const createUser = async () => {
-		try {
-			const resp = await fetch(url + '/users/' + username, {
-				method: 'POST',
-				header: {
-					'Content-type': 'application/json'
-				},
-			});
-			if (!resp, ok)
-				throw new Error("Something went wrong with username");
-			const data = await resp.json();
-			getDataAsync();
-		} catch (error) {
-			console.error('error');
-		}
-	}
 
 
-	const handleSubmit = async e => {
+	const handleSubmit = e => {
 		e.preventDefault();
-		try {
-			const resp = await fetch(url + '/todos/' + username, {
-				method: 'POST',
-				header: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify({
-					label: tarea,
-					is_done: false,
-				})
-			});
-			if (!resp, ok)
-				throw new Error("Something went wrong with Tarea");
-			const data = await resp.json();
-			getDataAsync();
-			setTarea('')
-		} catch (error) {
-			console.error('error');
-		}
 	}
 
-	const handleDelete = async (todo_id) => {
-		try {
-			const resp = await fetch(url + '/todos/' + todo_id, {
-				method: 'DELETE',
-			});
-			if (!resp, ok)
-				throw new Error("Something went wrong with username");
-			getDataAsync();
-		} catch (error) {
-			console.error('error');
-		}
-	}
 
-	const HandleChange = e => setWork(e.target.value);
+	//POST//
 
-	const handleKeyDown = e => {
+	const handleKeyDown = async e => {
 		if (e.key === "Enter") {
 			setTarea([...tarea, work]);
 			setWork("");
 		};
-	};
 
+		//POST//
 
-	useEffect(( )=> {
-		getDataAsync()
-	},[tarea])
+		const createUser = async () => {
+			try {
+				const data = await fetch(url + '/users/' + username, {
+					method: "POST",
+					headers: {
+						'content-type': 'application/json'
+					},
+				});
+				if (!resp.ok) throw new Error('something went wrong creating them username');
+				getDataAsync();
+			} catch (error) {
+				console.error(error);
+			}
+		}
 
-	return (
-		<div className="fondo">
-			<div className="container">
-				<div className="text-center">
-					<h1>TODO LIST</h1>
-					<input type="text" onChange={HandleChange} onKeyDown={(e) => handleKeyDown(e)} value={work} />
-					<ul>
-						{tarea.map((item, index) => (
-							<div className="items" key={index}>
-								<li>{item}</li>
-								<button onClick={() => { handleDelete(index); }}>X</button>
-							</div>))}
-					</ul>
-					<p className="item">{tarea.length} {tarea.length === 1 ? "item" : "items"}</p>
+		//DELETE//
+		const handleDelete = async (todo_id) => {
+			try {
+				const resp = await fetch(url + '/todos/' + todo_id, {
+					method: 'DELETE',
+				});
+				if (!resp, ok)
+					throw new Error("Something went wrong with username");
+				getDataAsync();
+			} catch (error) {
+				console.error('error');
+			}
+		}
+
+		const HandleChange = e => setWork(e.target.value);
+
+		useEffect(() => {
+			getDataAsync()
+		}, [tarea])
+
+		return (
+			<div className="fondo">
+				<div className="container">
+					<div className="text-center">
+						<h1>TODO LIST</h1>
+						<input type="text" onChange={HandleChange} onKeyDown={(e) => handleKeyDown(e)} value={work} />
+						<ul>
+							{tarea.map((item, index) => (
+								<div className="items" key={index}>
+									<li>{item}</li>
+									<button onClick={() => { handleDelete(index); }}>X</button>
+								</div>))}
+						</ul>
+						<p className="item">{tarea.length} {tarea.length === 1 ? "item" : "items"}</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
 
+	 }
+	}
 export default Home;
